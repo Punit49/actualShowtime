07dc8da86d07aeddb5f760338bd2c0e5aed70c87
Punit Sahu <therealpunit5@gmail.com>
2025-08-14 20:17:04 +0530

    Update project with latest changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/showtime`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/showtime2`)
     } catch (error) {
         console.log(error.message);
         
