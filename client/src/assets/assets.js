9e807545a7258acba1b229979a921ce7093acd85
Punit Sahu <therealpunit5@gmail.com>
2025-08-14 19:59:49 +0530

    Update project with latest changes

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -4,7 +4,7 @@ import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
 import screenImage from './screenImage.svg'
 import profile from './profile.png'
-import Preview from './preview.png'
+import Preview from './Preview.png'
 
 export const assets = {
     logo,
